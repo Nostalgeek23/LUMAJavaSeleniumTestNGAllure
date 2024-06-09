e465d039a4009e80c282cb463e108f37a317de33
Eugene <dorick23@gmail.com>
2024-06-09 16:56:20 +0300

    add Allure reports

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,9 +1,15 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 
     @Test
-    public void testtest() {
+    public void testPass() {
+        Assert.assertTrue(true);
+    }
 
+    @Test
+    public void testFail() {
+        Assert.assertTrue(false);
     }
 }
