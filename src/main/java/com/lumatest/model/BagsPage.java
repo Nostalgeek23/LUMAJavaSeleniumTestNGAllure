32ac3605c22355c35988c0b827133eb24486e218
Eugene <dorick23@gmail.com>
2024-06-20 23:00:50 +0300

    remove imports

--- src/main/java/com/lumatest/model/BagsPage.java
+++ src/main/java/com/lumatest/model/BagsPage.java
@@ -1,9 +1,6 @@
 package com.lumatest.model;
 
-import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class BagsPage extends CategoryPage{
 
